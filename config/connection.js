bc29cc93d73c6b31e22373858fb765d6f91d6e28
orangeblack13 <AndrewJKleiner@gmail.com>
2020-07-15 18:35:26 -0400

    JawsDB Info Added

--- config/connection.js
+++ config/connection.js
@@ -4,23 +4,27 @@ var mysql = require("mysql");
 //Setup Password Path
 require("dotenv").config();
 
-//MySQL Connection Data
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.DB_PASS,
-  database: "burgersDB"
-});
+//MySQL Connection Data + JawsDB Data
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.DB_PASS,
+    database: "burgersDB"
+  });
+}
 
 //Make connection
-connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
 //Export Connection for ORM
-  module.exports = connection;
+module.exports = connection;
